27d51deb144c8371c16dde4e208f0fed1fd08f3d
NadaRamadan <nada7.ra@gmail.com>
2025-05-15 15:34:36 +0300

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembership.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string AddressFirstLine { get; set; }
+
+        public string AddressSecondLine { get; set; }
+
+        public string AddressCity { get; set; }
+
+        public string PostCode { get; set; }
+    }
+}
